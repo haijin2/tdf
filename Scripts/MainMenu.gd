c59a061211e80cacc5bb4fa5f9d4d1033452c2ee
haijin2 <94418542+haijin2@users.noreply.github.com>
2023-11-03 14:55:48 +0800

    tdf
    final commit

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Scenes/World.tscn")
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
